288833254f00354a29920f62add0851af2534801
Bhone Pyae Aung <bhonepyaeaung15@gmail.com>
2021-08-16 11:58:16 +0630

    added users component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import UserItem from "./components/users/UserItem";
+import Users from "./components/users/Users";
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
         <Navbar />
-        <UserItem />
+        <div className='container'>
+          <Users />
+        </div>
       </div>
     );
   }
